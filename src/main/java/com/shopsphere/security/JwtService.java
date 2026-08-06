d8d68f7f9e4d13b76004b19471bcd0504c352f38
Vishal-0305 <vishal.choudhary1716@gmail.com>
2026-08-07 05:05:56 +0530

    Implement JWT token generation

--- src/main/java/com/shopsphere/security/JwtService.java
+++ src/main/java/com/shopsphere/security/JwtService.java
@@ -2,15 +2,18 @@ package com.shopsphere.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
     private static final String SECRET =
-            "mysecretkeymysecretkeymysecretkeymysecretkey";
+            "ZmE0NTk5NjA4M2Y4ZjQwMzk3MGE5NTY3NWE2NzA5YmU3YjQxNmM5ZjU0YTU4Y2IwNmI4ZjA4NDQ2NmY0NTU4ZA==";
 
     public String generateToken(String email) {
 
@@ -18,7 +21,37 @@ public class JwtService {
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(getSignKey())
                 .compact();
     }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token, String email) {
+        return extractEmail(token).equals(email) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+
+    private SecretKey getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
 }
