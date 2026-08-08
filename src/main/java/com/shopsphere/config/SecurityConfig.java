d42143905885d92732a22a1855ed73617290feee
Vishal-0305 <vishal.choudhary1716@gmail.com>
2026-08-08 13:58:13 +0530

    Implement JWT authentication foundation

--- src/main/java/com/shopsphere/config/SecurityConfig.java
+++ src/main/java/com/shopsphere/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.shopsphere.config;
 
+import com.shopsphere.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,6 +26,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/users/register","/api/v1/auth/login").permitAll()
                         .anyRequest().authenticated()
                 );
+        http.addFilterBefore(jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
